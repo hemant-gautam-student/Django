c2ce9134fbb1835e104c8fa9f117b957b8cf09e1
hemant-gautam-student <hemantgautamstudent@gmail.com>
2024-07-25 04:27:29 +0530

    Experiment 2

--- MyFirstProject/calc/views.py
+++ MyFirstProject/calc/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'home.html',{'name':'Raju'})
+
+def add(request):
+    val1=int(request.POST['num1'])
+    val2=int(request.POST['num2'])
+    val3=val1+val2
+    name = request.POST["username"]
+    return render(request,'results.html',{'result':val3, 'username':name})
+ 
+# def inputname(request):
+    
